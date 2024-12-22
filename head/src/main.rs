5fc37c439d1868141095740cf96c333d6a04b23c
adnan007d <adnanmansuri54@gmail.com>
2024-12-22 17:38:44 +0000

    perf: Removed String allocation and using buf reader lazy method

--- head/src/main.rs
+++ head/src/main.rs
@@ -19,45 +19,33 @@ fn main() -> io::Result<()> {
     match args.path {
         Some(path) => {
             // Check if file exists
-            let metadata = fs::metadata(&path).unwrap_or_else(|_| {
-                eprintln!("{0} does not exists", path);
-                process::exit(1);
-            });
-
+            let metadata = fs::metadata(&path)?;
             // Shouldn't be a directory
             if metadata.is_dir() {
                 eprintln!("{0} is a directory", path);
                 process::exit(1);
             }
-            head(fs::File::open(path)?, args.number)
+            head(fs::File::open(path)?, args.number)?
         }
-        None => head(std::io::stdin(), args.number),
+        None => head(std::io::stdin(), args.number)?,
     }
 
     Ok(())
 }
 
-fn head<R: Read>(reader: R, n: usize) {
-    let mut buf = String::new();
-    let mut buf_reader = BufReader::new(reader);
-    for _ in 0..n {
-        buf.clear();
-        match buf_reader.read_line(&mut buf) {
-            Ok(0) => break,
-            Ok(_) => {
-                if let Err(e) = std::io::stdout().write_all(buf.as_bytes()) {
-                    eprintln!("Error while writing to stdout {}", e);
-                    break;
-                }
+fn head<R: Read>(reader: R, n: usize) -> io::Result<()> {
+    let buf_reader = BufReader::new(reader);
+    for line in buf_reader.lines().take(n) {
+        match line {
+            Ok(line) => {
+                std::io::stdout().write_all(line.as_bytes())?;
+                std::io::stdout().write_all(b"\n")?;
             }
-            Err(e) => {
-                eprintln!("Error reading from input {}", e);
-                break;
-            }
+            Err(e) => Err(e)?,
         }
     }
 
-    if let Err(e) = std::io::stdout().flush() {
-        eprintln!("Error flushing stdout: {}", e);
-    }
+    std::io::stdout().flush()?;
+
+    Ok(())
 }
